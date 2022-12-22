e028d6f9f6510c4daca7f192bece0acceca47762
Aleksei Ivanov <aleksei.ivanov@rakuten.com>
2022-12-22 10:38:22 +0900

    initial commit

---
+++ src/tg_clj_rss/core.clj
@@ -0,0 +1,7 @@
+(ns tg-clj-rss.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/tg_clj_rss/core_test.clj
@@ -0,0 +1,7 @@
+(ns tg-clj-rss.core-test
+  (:require [clojure.test :refer :all]
+            [tg-clj-rss.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
